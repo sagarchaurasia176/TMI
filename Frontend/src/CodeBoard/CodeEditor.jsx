2f4450409dacaa2a5f88ec5e3781e9443472b6f5
sagarchaurasia176 <sagarchaurasia176@gmail.com>
2024-05-08 01:07:26 +0530

    code updated

--- Frontend/src/CodeBoard/CodeEditor.jsx
+++ Frontend/src/CodeBoard/CodeEditor.jsx
@@ -4,6 +4,7 @@ const CodeEditor = () => {
   return (
     <div>
             <p>code editor part Here</p>
+            <h1>start work</h1>
         {/* everyone write the code here for making the code editor */}
     </div>
   )
