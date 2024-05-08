02bc413838ac8ac49b1a822471baf347610985f9
sagarchaurasia176 <sagarchaurasia176@gmail.com>
2024-05-09 00:32:00 +0530

    Code

--- Frontend/src/CodeBoard/CodeEditor.jsx
+++ Frontend/src/CodeBoard/CodeEditor.jsx
@@ -4,9 +4,7 @@ import React from 'react'
 const CodeEditor = () => {
   return (
     <div>
-             <h1>Hi this side sagar and aditya</h1>
-        {/* everyone write the code here for making the code editor */}
-        <p>final demo</p>
+            
     </div>
   )
 }
