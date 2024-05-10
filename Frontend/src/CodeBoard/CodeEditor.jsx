d09249ab4e789eb38bf4e4349b4db278f28836c5
TwoDarray <dhruvup444@gmail.com>
2024-05-10 14:20:41 +0530

    test1msg

--- Frontend/src/CodeBoard/CodeEditor.jsx
+++ Frontend/src/CodeBoard/CodeEditor.jsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box } from "@chakra-ui/react";
 import Editor from '@monaco-editor/react';
 
 const CodeEditor = () => {
+  const [value, setValue] = useState("");
   return (
     <Box height="100%" width="100%">
       <Editor 
         height="100%"
         width="100%"
         defaultLanguage="javascript"
-        defaultValue="// some comment"
+        defaultValue="// write your code below"
+        value={value}
+        onChange={(value) => setValue(value)}
       />
     </Box>
   );
